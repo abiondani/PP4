c63a9a21201e108ebead4eecb540a2bd29a650d5
Luciano Lopez <lucianolop88@gmail.com>
2025-06-22 20:42:00 -0300

    Agregado el dockerfile del frontend de administradores

--- agendar-frontend/src/paginas/Notificar.js
+++ agendar-frontend/src/paginas/Notificar.js
@@ -9,7 +9,7 @@ const Notificar = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:3000/api/notificaciones", {
+      const response = await fetch(process.env.REACT_APP_API_NOTIFICACIONES, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
