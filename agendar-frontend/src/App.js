d9f7896fa017a64d0bda3d0ae027aad995670b8a
Luciano Lopez <lucianolop88@gmail.com>
2025-06-22 21:19:15 -0300

    Arreglado ruta para la encuesta

--- agendar-frontend/src/App.js
+++ agendar-frontend/src/App.js
@@ -7,6 +7,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<PaginaPrincipal />} />
+        <Route path="/encuesta/:token" element={<Encuesta />} />
       </Routes>
     </Router>
   );
